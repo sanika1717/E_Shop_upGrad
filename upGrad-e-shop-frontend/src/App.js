7122a6c81f4d2be6c15d9a9fd690fc1576936e18
SanikaBhosale <130919957+sanika1717@users.noreply.github.com>
2023-07-30 20:09:47 +0530

    Add files via upload

---
+++ upGrad-e-shop-frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import Home from './components/home/Home';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Home></Home>
+
+    </div>
+  );
+}
+
+export default App;
